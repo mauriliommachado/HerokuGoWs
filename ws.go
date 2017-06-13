778719678ba84a87902d80b599bd2c62f19724ad
mauriliommachado <mauriliommachado@gmail.com>
2017-06-12 21:42:31 -0300

    initial commit

--- ws.go
+++ ws.go
@@ -5,21 +5,25 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func HelloServer(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "hello, "+req.URL.Query().Get(":name")+"!\n")
 }
 
-
+func HelloWorld(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "Hello World!")
+}
 
 func main() {
 	m := pat.New()
+	m.Get("/", http.HandlerFunc(HelloWorld))
 	m.Get("/hello/:name", http.HandlerFunc(HelloServer))
 	// Register this pat with the default serve mux so that other packages
 	// may also be exported. (i.e. /debug/pprof/*)
 	http.Handle("/", m)
-	err := http.ListenAndServe(":12345", nil)
+	err := http.ListenAndServe(os.Getenv("PORT"), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
